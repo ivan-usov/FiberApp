aa5fe9f2257dfdc41c06518cc3f5cb215af0895d
Ivan Usov <ivan.a.usov@gmail.com>
2016-03-11 17:59:49 +0100

    Specify the path to +panel folder in order to avoid conflicts with other +panel folders existing on matlab path.

--- FiberApp.m
+++ FiberApp.m
@@ -46,7 +46,7 @@ CreateMenu;
 CreateToolbar;
 
 % Load panels from the '+panel' folder
-panel_files = what('+panel');
+panel_files = what(['FiberApp' filesep '+panel']);
 [~, panel_names] = cellfun(@fileparts, panel_files.m, 'UniformOutput', false);
 for k = 1:length(panel_names)
     panel_name = panel_names{k};
